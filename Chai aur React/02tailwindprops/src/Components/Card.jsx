03f96cf3ac303d07cbf868f45ddb2e2ccfe01ca8
bibekghosh <bibek7478@gmail.com>
2023-09-10 18:51:01 +0530

    props and tailwind

---
+++ Chai aur React/02tailwindprops/src/Components/Card.jsx
@@ -0,0 +1,35 @@
+// import React from 'react'
+import PropTypes from 'prop-types';
+
+const Card = ({username="Mark", btnText}) => {
+  return (
+    <div className="relative h-[400px] w-[300px] rounded-md">
+  <img
+    src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+    alt="AirMax Pro"
+    className="z-0 object-cover w-full h-full rounded-md"
+  />
+  <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+  <div className="absolute text-left bottom-4 left-4">
+    <h1 className="text-lg font-semibold text-white">{username}</h1>
+    <p className="mt-2 text-sm text-gray-300">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      debitis?
+    </p>
+    <button className="inline-flex items-center mt-2 text-sm font-semibold text-white cursor-pointer">
+      {btnText} →
+    </button>
+  </div>
+</div>
+
+  )
+}
+
+Card.propTypes = {
+    username: PropTypes.string.isRequired, // Use the appropriate type (string, number, etc.) and validation
+  };
+Card.propTypes = {
+    btnText: PropTypes.string.isRequired, // Use the appropriate type (string, number, etc.) and validation
+  };
+
+export default Card;
